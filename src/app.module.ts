bb9aaf16a7865d13f7eea9bfe9289cd060bb527b
luckyhyom <bs_khm@naver.com>
2021-11-24 23:58:20 +0900

    Chore: sqlite3 설치
    - sqlite3 설치
    - TypeORM sqlite3 설정
    - config폴더 생성
    - database폴더 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SQLiteConfig } from './config/database/sqlite3/configuration';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(SQLiteConfig)],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/config/database/sqlite3/configuration.ts
@@ -0,0 +1,10 @@
+import { TypeOrmModuleOptions } from "@nestjs/typeorm";
+
+export const SQLiteConfig: TypeOrmModuleOptions = {
+    type: 'sqlite',
+    database: ':memory:',
+    entities: [__dirname + '/../**/*.entity.{js,ts}'],
+    synchronize: true,
+    logging: true,
+    keepConnectionAlive: true,
+}
