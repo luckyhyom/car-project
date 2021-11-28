19e43f57a707ce087a4b09e1c48a259d263567bc
luckyhyom <bs_khm@naver.com>
2021-11-29 00:08:37 +0900

    Chore: limitArrayPipe 설명 주석 추가, 사용되지 않는 import 제거

--- src/common/pipes/limit-array.pipe.ts
+++ src/common/pipes/limit-array.pipe.ts
@@ -1,6 +1,14 @@
 import { HttpException, HttpStatus, ParseArrayPipe } from "@nestjs/common";
 
 export class LimitArrayPipe extends ParseArrayPipe {
+
+    /**
+     * 
+     * @param options
+     * options: { items: CreateDTO }, 1, 5
+     * @param min 
+     * @param max 
+     */
     constructor(options = {}, private min: number, private max: number) {
         super(options)
     }
@@ -8,10 +16,10 @@ export class LimitArrayPipe extends ParseArrayPipe {
     async transform(value, metadata) {
         if (Array.isArray(value)) {
             if(value.length < this.min) {
-                throw new HttpException(`Array length is longer than ${this.min}`, HttpStatus.BAD_REQUEST);
+                throw new HttpException(`It should be not less than ${this.min}`, HttpStatus.BAD_REQUEST);
             }
             if(value.length > this.max) {
-                throw new HttpException(`Array length is shorter than ${this.max}`, HttpStatus.BAD_REQUEST);
+                throw new HttpException(`It should be not more than ${this.max}`, HttpStatus.BAD_REQUEST);
             }
         }
         return await super.transform(value, metadata);
